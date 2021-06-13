fb71deb84657c3b9da41c5e28a10ff2a61bb403e
Aby Kal <abyabrkal@gmail.com>
2021-06-13 12:10:04 +0400

    Create Order button implemented with Form and data POST to db

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import *
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'    # or use a selected fields in a list
+
