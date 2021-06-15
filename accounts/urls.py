0ce8ceb578977dee8e2026eb032c5db602b1ccbb
Aby Kal <abyabrkal@gmail.com>
2021-06-15 09:53:18 +0400

    Register user page for adding users.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
     path('products/', views.products, name='products'),
     path('customer/<str:pk>', views.customer, name='customer'),
 
